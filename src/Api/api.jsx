7e8ea4416bdb0e05f7fd1a33dce652cfb1f52a42
husanovjamshid <husanovjamshid11@gmail.com>
2023-02-20 18:13:11 +0500

    change server

--- src/Api/api.jsx
+++ src/Api/api.jsx
@@ -3,5 +3,5 @@ import axios from "axios";
 const BASE_URL = "http://books.ogaw.uz";
 
 export const api = {
-  userRegister: (value) => axios.post(BASE_URL + "/user/register", value),
+  userRegister: (value) =>  axios.post(BASE_URL + "/user/register", value),
 };
